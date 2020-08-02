028474c51a6883f5c79d62e38a14c294e6a4632b
Naman Singhal <prince.naman1999@gmail.com>
2020-08-02 17:22:22 +0530

    v0.2 Enemy created

--- main.py
+++ main.py
@@ -6,56 +6,115 @@ screen_height = 480
 # setting window size.
 win = pygame.display.set_mode((screen_width, screen_height))
 # game window title
-pygame.display.set_caption("Goblin Hunter - 1st game")
+pygame.display.set_caption("Goblin Hunter")
 
 # loading character movements
-walkRight = list(map(pygame.image.load,
-                     '{folder}R1.png {folder}R2.png {folder}R3.png {folder}R4.png {folder}R5.png {folder}R6.png {folder}R7.png {folder}R8.png {folder}R9.png'.format(folder='img/').split()))
-walkLeft = list(map(pygame.image.load,
-                    '{folder}L1.png {folder}L2.png {folder}L3.png {folder}L4.png {folder}L5.png {folder}L6.png {folder}L7.png {folder}L8.png {folder}L9.png'.format(folder='img/').split()))
 bg = pygame.image.load('img/bg.jpg')
 idle = pygame.image.load('img/standing.png')
 
 
 class player:
-    def __init__(self, x, y, width, height):
+    walkRight = list(map(pygame.image.load,
+                         '{folder}R1.png {folder}R2.png {folder}R3.png {folder}R4.png {folder}R5.png {folder}R6.png {folder}R7.png {folder}R8.png {folder}R9.png'.format(folder='img/').split()))
+    walkLeft = list(map(pygame.image.load,
+                        '{folder}L1.png {folder}L2.png {folder}L3.png {folder}L4.png {folder}L5.png {folder}L6.png {folder}L7.png {folder}L8.png {folder}L9.png'.format(folder='img/').split()))
+
+    def __init__(self, x, y, width, height, jump_height=10, vel=8):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
-        self.vel = 8
+        self.vel = vel
         self.left = False
         self.right = False
         self.walkCount = 0
-        self.jumpcount = 10
+        self.jumpcount = jump_height
         self.jump = False
+        self.standing = True
 
     def draw(self, win):
         # since we have only 9 sprites/movement we set 3 frame = 1 image and walkCount <= 27
         if (self.walkCount+1) >= 27:
             self.walkCount = 0
-        # logic to choose character image as per movements
-        if self.left:
-            win.blit(walkLeft[self.walkCount//3], (self.x, self.y))
-            self.walkCount += 1
-        elif self.right:
-            win.blit(walkRight[self.walkCount//3], (self.x, self.y))
+
+        if not(self.standing):
+            # logic to choose character image as per movements
+            if self.left:
+                win.blit(self.walkLeft[self.walkCount//3], (int(self.x), int(self.y)))
+                self.walkCount += 1
+            elif self.right:
+                win.blit(self.walkRight[self.walkCount//3], (int(self.x), int(self.y)))
+                self.walkCount += 1
+        else:
+            if self.right:
+                win.blit(self.walkRight[0], (int(self.x), int(self.y)))
+            else:
+                win.blit(self.walkLeft[0], (int(self.x), int(self.y)))
+
+
+class fire_bullet:
+    def __init__(self, x, y, radius, color, facing, vel=10):
+        self.x, self.y, self.facing = x, y, facing
+        self.color = color
+        self.radius = radius
+        self.vel = vel*facing
+
+    def draw(self, win):
+        pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
+
+
+class enemy:
+    walkRight = list(map(pygame.image.load,
+                         '{folder}R1E.png {folder}R2E.png {folder}R3E.png {folder}R4E.png {folder}R5E.png {folder}R6E.png {folder}R7E.png {folder}R8E.png {folder}R9E.png {folder}R10E.png {folder}R11E.png'.format(folder='img/').split()))
+    walkLeft = list(map(pygame.image.load,
+                        '{folder}L1E.png {folder}L2E.png {folder}L3E.png {folder}L4E.png {folder}L5E.png {folder}L6E.png {folder}L7E.png {folder}L8E.png {folder}L9E.png {folder}L10E.png {folder}L11E.png'.format(folder='img/').split()))
+
+    def __init__(self, x, y, width, height, end, vel=4):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.end = end
+        self.vel = vel
+        self.walkCount = 0
+        self.path = [self.x, self.end]
+
+    def draw(self, win):
+        self.move()
+        if (self.walkCount+1) >= 33:
+            self.walkCount = 0
+
+        if self.vel > 0:
+            win.blit(self.walkRight[self.walkCount//3], (int(self.x), int(self.y)))
             self.walkCount += 1
         else:
-            win.blit(idle, (self.x, self.y))
+            win.blit(self.walkLeft[self.walkCount//3], (int(self.x), int(self.y)))
+            self.walkCount += 1
+
+    def move(self):
+        if self.x+self.vel in range(self.path[0], self.path[1]):
+            self.x += self.vel
+        else:
+            self.vel = self.vel * -1
+            self.walkCount = 0
 
 
-clock = pygame.time.Clock()
-
-
-def redrawGameWindow():
+def redrawGameWindow():  # function to render window
     win.blit(bg, (0, 0))
-    hero.draw(win)
+    hero.draw(win)  # hero
+    goblin.draw(win)
+    for bullet in bullets:  # bullets
+        bullet.draw(win)
     pygame.display.update()
 
 
-hero = player(20, 416, 64, 64)
+clock = pygame.time.Clock()
+jump_height = 8
+hero = player(20, 416, 64, 64, jump_height, vel=6)
+goblin = enemy(0, 420, 64, 64, screen_width-50)
 run = True
+bullets = []
+
 # game begins
 while run:
     clock.tick(27)  # 27fps. 9 images per movement. 1 move = 3 frames
@@ -65,26 +124,41 @@ while run:
             run = False
     keys = pygame.key.get_pressed()
 
+    for bullet in bullets:  # fire bullets
+        if bullet.x in range(0, screen_width):
+            bullet.x += bullet.vel
+        else:  # delete when beyond screen
+            bullets.pop(bullets.index(bullet))
+
+    if keys[pygame.K_SPACE] and len(bullets) < 6:
+        if hero.left:
+            facing = -1
+        else:
+            facing = 1
+        bullets.append(fire_bullet(
+            round(hero.x+hero.width//2),
+            round(hero.y+hero.height//2),
+            4, (0, 0, 0), facing))
+
     if keys[pygame.K_LEFT] and hero.x > 0:  # movement control
         hero.x -= hero.vel
         hero.left, hero.right = True, False
-        print("L")
+        hero.standing = False
     elif keys[pygame.K_RIGHT] and hero.x < (screen_width-hero.width-hero.vel):
         hero.x += hero.vel
         hero.left, hero.right = False, True
-        print("R")
+        hero.standing = False
     else:
-        hero.left, hero.right = False, False
         hero.walkCount = 0
+        hero.standing = True
 
     if not(hero.jump):  # Logic to make a jump and fall back
-        if keys[pygame.K_SPACE]:
+        if keys[pygame.K_UP]:
             hero.jump = True
             hero.left, hero.right = False, False
             hero.walkCount = 0
-            print("JUMP")
     else:
-        if hero.jumpcount >= -10:
+        if hero.jumpcount >= -(jump_height):
             neg = 1
             if hero.jumpcount < 0:
                 neg = -1
@@ -92,7 +166,7 @@ while run:
             hero.jumpcount -= 1
         else:
             hero.jump = False
-            hero.jumpcount = 10
+            hero.jumpcount = jump_height
 
     redrawGameWindow()
 
